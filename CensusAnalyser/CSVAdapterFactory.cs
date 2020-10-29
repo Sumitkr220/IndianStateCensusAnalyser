0cddbe1a9d4109c19ce9edcb720eb851c2ab99e4
sumitpandit <sumitpandit178@gmail.com>
2020-10-29 07:59:17 +0530

    [Sumit] Refactor: Ability to load census information usnig CSV file

---
+++ CensusAnalyser/CSVAdapterFactory.cs
@@ -0,0 +1,24 @@
+﻿using CensusAnalyserLive.DTO;
+using CensusAnalyserLive.POCO;
+using CensusAnalyserLive;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserLive
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyzer.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyzer.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+
+}
---
+++ CensusAnalyser/CensusAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using CensusAnalyserLive.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserLive.DTO
+{
+    public class CensusAnalyzer
+    {
+        public enum Country
+        {
+            INDIA,
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+
+}
+
